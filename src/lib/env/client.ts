905bc74befa04d31aeaf3f796c5e623df1330ebc
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2025-08-01 19:05:42 +0300

    clean up env files

--- src/lib/env/client.ts
+++ src/lib/env/client.ts
@@ -1,11 +1,8 @@
-import { z } from "zod";
 import { createEnv } from "@t3-oss/env-core";
 
 export const env = createEnv({
   clientPrefix: "VITE_",
-  client: {
-    VITE_BETTER_AUTH_URL: z.url().default("http://localhost:3000"),
-  },
+  client: {},
   runtimeEnv: import.meta.env,
   emptyStringAsUndefined: true,
 });
--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -1,16 +1,7 @@
-import { z } from "zod";
 import { createEnv } from "@t3-oss/env-core";
 
 export const env = createEnv({
-  server: {
-    DB_FILE_NAME: z.string(),
-
-    BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string(),
-
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
-  },
-  runtimeEnv: import.meta.env,
+  server: {},
+  runtimeEnv: process.env,
   emptyStringAsUndefined: true,
 });
